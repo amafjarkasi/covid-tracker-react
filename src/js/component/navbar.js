d0f69b4aa0dc2c21e6e209736f45ad0250440a6b
amafjarkasi <amafjarkasi@gmail.com>
2021-06-06 20:35:06 +0000

    latest updates

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-dark bg-dark mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">COVID-19 - Data Tracker</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button type="button" className="btn btn-danger position-relative">
-						More data{" "}
-						<span className="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-primary p-2">
-							<span className="visually-hidden" />
-						</span>
-					</button>
+		<>
+			<nav className="navbar navbar-dark bg-dark mb-3">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">COVID-19 - Data Tracker</span>
 				</Link>
-			</div>
-		</nav>
+				<div className="ml-auto">
+					{/* <Link to="/demo"> */}
+					<DropdownButton variant="danger" id="dropdown-basic-button" title="Other Options">
+						<LinkContainer to="/demo">
+							<Dropdown.Item>Action</Dropdown.Item>
+						</LinkContainer>
+						<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+					</DropdownButton>
+					{/* </Link> */}
+				</div>
+			</nav>
+		</>
 	);
 };
